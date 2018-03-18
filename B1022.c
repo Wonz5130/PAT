1e859c63658bdd396a7acd678a180ed1d886c4e2
王舟 <wangzhou5130@gmail.com>
2018-03-18 20:19:38 +0800

    D进制的A+B

---
+++ B1022.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int main(){
+    //char A,B,D;
+    long long A,B; //A+B的范围可能会超过int范围(<=2^31-1)
+    int D,i=0;
+    int a[100];
+    //sscanf(str,"%d",&A);
+    //sscanf(str,"%d",&B);
+    //scanf("%d",&D);
+    scanf("%lld%lld%d",&A,&B,&D);
+    A=A+B;
+    do{
+        a[i++]=A%D;
+        A=A/D;
+    }
+    while(A!=0);  //先执行，防止一上来A+B就是0的情况
+    for(i=i-1;i>=0;i--){
+        printf("%d",a[i]);
+    }
+    return 0;
+}
+
+
+/*
+#include<stdio.h>
+int main(){
+    //char A,B,D;
+    int A,B,D,i=0;
+    int a[100];
+    //sscanf(str,"%d",&A);
+    //sscanf(str,"%d",&B);
+    //scanf("%d",&D);
+    scanf("%d%d%d",&A,&B,&D);
+    A=A+B;
+    while(A!=0){
+        a[i++]=A%D;
+        A=A/D;
+    }
+    for(i=i-1;i>=0;i--){
+        printf("%d",a[i]);
+    }
+    return 0;
+}
+*/

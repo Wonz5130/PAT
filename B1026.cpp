b34b364616491e01cbb306b5aeddf309e4a00699
王舟 <wangzhou5130@gmail.com>
2018-07-03 21:12:45 +0800

    Update B1026.cpp

--- B1026.cpp
+++ B1026.cpp
@@ -1,3 +1,5 @@
+/*
+//1st
 #include<cstdio>
 int main(){
     long long c1,c2;
@@ -9,4 +11,26 @@ int main(){
     else c1=c1/100;
     printf("%02lld:%02lld:%02lld",c1/3600,c1/60%60,c1%60);  // 注意不足两位时高位用0补充
     return 0;
+}
+*/
+
+
+//2nd
+#include<stdio.h>
+int main(){
+	long long C1,C2;
+	int second,minute,hour;
+	scanf("%lld%lld",&C1,&C2);
+	C2 = C2-C1;
+	if(C2 % 100 >= 50){
+		C2 = C2/100 + 1;
+	}
+	else{
+		C2 = C2/100;
+	}
+	second = C2%60;
+	minute = C2/60%60;
+	hour = C2/3600;
+	printf("%02d:%02d:%02d",hour,minute,second);
+	return 0;
 }

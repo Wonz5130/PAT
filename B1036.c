a8bedbe6cf3669a363caff0b42940edb0f205188
王舟 <wangzhou5130@gmail.com>
2018-07-08 12:57:43 +0800

    二刷

--- B1036.c
+++ B1036.c
@@ -29,6 +29,9 @@ int main(){
 }
 */
 
+
+/*
+//1st
 #include<stdio.h>
 int main(){
     int i,j,col,row;
@@ -55,5 +58,38 @@ int main(){
         printf("%c",c);
     }
     printf("\n");
+    return 0;
+}
+*/
+
+
+//2nd
+#include<stdio.h>
+int main(){
+    int n,row;
+    char c;
+    scanf("%d %c",&n,&c);
+    if(n%2 == 1){
+        row = n/2+1;
+    }
+    else row = n/2;
+    for(int i = 0;i < row;i ++){
+        if(i == 0){
+            for(int j = 0;j < n;j ++){
+                printf("%c",c);
+            }
+            printf("\n");
+        }
+        else if(i == row-1){   //这里少一个else就是错的,会多输出一行
+            for(int j = 0;j < n;j ++){
+                printf("%c",c);
+            }
+        }
+        else{
+            printf("%c",c);
+            for(int j = 0;j < n-2;j ++) printf(" ");
+            printf("%c\n",c);
+        }
+    }
     return 0;
 }
